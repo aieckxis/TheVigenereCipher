6b1c2b4ee3e214c241b6b04a48a9365793d393ad
aieckxis <aleckxiskate@gmail.com>
2023-04-16 13:22:09 +0800

    asked the user to input

--- the vigenere cipher.py
+++ the vigenere cipher.py
@@ -3,6 +3,8 @@
 
 # Problem 3
 # Ask the plaintext message and keyword from the user.
+plain_text = input("Enter the plaintext message:")
+keyword = input("Enter the keyword:")
 # Convert the keyword to corresponding letter values 0-25.
 # Apply the Vigenère cipher to generate the ciphertext.
 # Print the resulting ciphertext.
