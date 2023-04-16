eea095ea9f1850b2966c59fefc674f46e52f3158
aieckxis <aleckxiskate@gmail.com>
2023-04-16 13:05:54 +0800

    pseudocode

---
+++ the vigenere cipher.py
@@ -0,0 +1,8 @@
+# Aleckxis Kate V. Bernabe
+# BSCPE 1-4
+
+# Problem 3
+# Ask the plaintext message and keyword from the user.
+# Convert the keyword to corresponding letter values 0-25.
+# Apply the Vigenère cipher to generate the ciphertext.
+# Print the resulting ciphertext.
